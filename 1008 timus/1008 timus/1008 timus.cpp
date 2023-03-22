760eed4b0840e60afa7c16f84b6e3f3d6c9cb210
Lost-Fly <114507453+Lost-Fly@users.noreply.github.com>
2023-03-22 12:59:40 +0500

    Create 1008

---
+++ 1008 timus/1008 timus/1008 timus.cpp
@@ -0,0 +1,129 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Pair {
+public:
+	void SetCords(int x, int y) {
+		this->x_ = x;
+		this->y_ = y;
+	}
+
+	void ShowCords() {
+		cout << this->x_ << ' ' << this->y_ << endl;
+	}
+
+
+	int x_;
+	int y_;
+};
+
+
+
+class Neighbours {
+public:
+	bool top = false;
+	bool bottom = false;
+	bool right = false;
+	bool left = false;
+	Pair p_t, p_b, p_r, p_l;
+	bool mentioned = false;
+
+	void ShowNeib() {
+		if (true) {
+			string s = "";
+			if (right == true)
+				s += "R";
+			if (top == true)
+				s += "T";
+			if (bottom == true)
+				s += "B";
+			s += ",";
+			cout << s << endl;
+		}
+		else {
+			cout << "," << endl;
+		}
+
+	};
+};
+
+bool comparePairs(Pair& a, Pair& b) {
+	return a.y_ < b.y_;
+}
+
+void sortCordsByY(vector<Pair>& Cords) {
+	sort(Cords.begin(), Cords.end(), comparePairs);
+}
+
+int main() {
+
+	vector<Pair> Cords;
+	int cords_amount = 0;
+	cin >> cords_amount;
+	int x, y;
+
+	Neighbours* Neig = new Neighbours[cords_amount];
+
+	for (int i = 0; i < cords_amount; i++) {
+		Pair p;
+		cin >> x >> y;
+		p.SetCords(x, y);
+		Cords.push_back(p);
+	}
+
+	cout << endl;
+
+
+	for (int i = 0; i < cords_amount; i++) {
+		Cords[i].ShowCords();
+	}
+
+	cout << endl;
+
+	sortCordsByY(Cords);
+
+	for (int i = 0; i < cords_amount; i++) {
+		Cords[i].ShowCords();
+	}
+
+	cout << endl;
+
+
+	Cords[0].ShowCords();
+
+	for (int i = 0; i < cords_amount - 1 ; i++) {
+
+		for (int j = i + 1; j < cords_amount; j++) {
+			if ((Cords[i].x_ == Cords[j].x_) && (Cords[j].y_ - Cords[i].y_ == 1)) {
+				Neig[i].top = true;
+				Neig[i].p_t.x_ = Cords[j].x_;
+				Neig[i].p_t.y_ = Cords[j].y_;
+				Neig[j].mentioned = true;
+			}
+			if ((Cords[i].x_ == Cords[j].x_) && (Cords[i].y_ - Cords[j].y_ == 1)) {
+				Neig[i].bottom = true;
+				Neig[i].p_b.x_ = Cords[j].x_;
+				Neig[i].p_b.y_ = Cords[j].y_;
+				Neig[j].mentioned = true;
+			}
+			if ((Cords[j].x_ - Cords[i].x_ == 1) && (Cords[i].y_ == Cords[j].y_)) {
+				Neig[i].right = true;
+				Neig[i].p_r.x_ = Cords[j].x_;
+				Neig[i].p_r.y_ = Cords[j].y_;
+				Neig[j].mentioned = true;
+			}
+		}
+
+		Neig[i].ShowNeib();
+
+
+	}
+
+
+	cout << "." << endl;
+
+
+}
---
+++ test _1008/test _1008/test _1008.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+struct Pixel {
+    int x, y;
+    vector<Pixel*> neighbors;
+
+    bool operator<(const Pixel& other) const {
+        if (x == other.x) {
+            return y < other.y;
+        }
+        return x < other.x;
+    }
+};
+
+void dfs(Pixel* pixel, map<Pixel*, bool>& visited) {
+    visited[pixel] = true;
+    for (Pixel* neighbor : pixel->neighbors) {
+        if (!visited[neighbor]) {
+            dfs(neighbor, visited);
+        }
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<Pixel> pixels(n);
+    for (int i = 0; i < n; i++) {
+        cin >> pixels[i].x >> pixels[i].y;
+    }
+
+    // Build graph
+    for (int i = 0; i < n; i++) {
+        Pixel& pixel = pixels[i];
+        for (int j = i + 1; j < n; j++) {
+            Pixel& other = pixels[j];
+            if ((pixel.x == other.x && abs(pixel.y - other.y) == 1) ||
+                (pixel.y == other.y && abs(pixel.x - other.x) == 1)) {
+                pixel.neighbors.push_back(&other);
+                other.neighbors.push_back(&pixel);
+            }
+        }
+    }
+
+    // Find connected components
+    map<Pixel*, bool> visited;
+    vector<int> components;
+    for (Pixel& pixel : pixels) {
+        if (!visited[&pixel]) {
+            components.push_back(1);
+            dfs(&pixel, visited);
+        }
+    }
+
+    // Sort pixels by row
+    sort(pixels.begin(), pixels.end());
+
+    // Find bottom-left pixel
+    Pixel* start_pixel = &pixels[0];
+    for (int i = 1; i < n; i++) {
+        Pixel& pixel = pixels[i];
+        if (pixel.y < start_pixel->y || (pixel.y == start_pixel->y && pixel.x < start_pixel->x)) {
+            start_pixel = &pixel;
+        }
+    }
+
+    // Output second representation
+    cout << start_pixel->x << " " << start_pixel->y << endl;
+    for (Pixel& pixel : pixels) {
+        if (pixel.neighbors.empty()) {
+            cout << "." << endl;
+            continue;
+        }
+        sort(pixel.neighbors.begin(), pixel.neighbors.end());
+        for (Pixel* neighbor : pixel.neighbors) {
+            if (neighbor->x == pixel.x + 1) {
+                cout << "R";
+            }
+            else if (neighbor->y == pixel.y - 1) {
+                cout << "T";
+            }
+            else if (neighbor->x == pixel.x - 1) {
+                cout << "L";
+            }
+            else if (neighbor->y == pixel.y + 1) {
+                cout << "B";
+            }
+        }
+        cout << "." << endl;
+    }
+
+    return 0;
+}
